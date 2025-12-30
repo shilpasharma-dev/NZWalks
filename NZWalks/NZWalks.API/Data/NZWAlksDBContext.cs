57e400252c4aec43272f39f0b8d36bfc241f2c5a
ShilpaSharma <shilpa.mca@gmail.com>
2025-12-30 16:24:00 +0100

    New Model - Data is created and migrated to SQL Server

--- NZWalks/NZWalks.API/Data/NZWAlksDBContext.cs
+++ NZWalks/NZWalks.API/Data/NZWAlksDBContext.cs
@@ -13,5 +13,6 @@ namespace NZWalks.API.Data
         public DbSet<Difficulty> Difficulties { get; set; }
         public DbSet<Region> Regions { get; set; }
         public DbSet<Walk> Walks { get; set; }
+        public DbSet<Image> Images { get; set; }
     }
 }
--- NZWalks/NZWalks.API/Migrations/20251230152016_adding Image table.Designer.cs
+++ NZWalks/NZWalks.API/Migrations/20251230152016_adding Image table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NZWalks.API.Data;
 
@@ -11,9 +12,11 @@ using NZWalks.API.Data;
 namespace NZWalks.API.Migrations
 {
     [DbContext(typeof(NZWAlksDBContext))]
-    partial class NZWAlksDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251230152016_adding Image table")]
+    partial class addingImagetable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
