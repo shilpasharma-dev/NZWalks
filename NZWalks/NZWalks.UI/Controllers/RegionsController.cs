981d3dc9bc53286cdcdf0f5d3f25fb7ae0466fa6
ShilpaSharma <shilpa.mca@gmail.com>
2026-01-04 00:55:22 +0100

    UI consuming getting all regions without any criteria

---
+++ NZWalks/NZWalks.UI/Controllers/RegionsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models.DTO;
+
+namespace NZWalks.UI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDto> responseRegion = new List<RegionDto>();
+
+            try
+            {
+                //Get All Regions from API
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:7279/api/regions");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                responseRegion.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());                               
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return View(responseRegion);
+        }
+    }
+}
